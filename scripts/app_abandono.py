83ad52b61d4881d41c91717236e0d4de125f26f7
Carlos Fernando Cardenas Montoya <carlosfernando95@gmail.com>
2026-05-09 16:04:15 -0500

    Nuevas lineas en rama vscode

--- scripts/app_abandono.py
+++ scripts/app_abandono.py
@@ -1,1 +1,8 @@
 print('Todo por desarrollar')\n\ndf['segmento_valor'] = pd.qcut(df['valor'], 3, labels=['bajo', 'medio', 'alto'])
+
+print('Todo por desarrollar')
+print('Todo por desarrollar')
+print('Todo por desarrollar')
+print('Todo por desarrollar')
+print('Todo por desarrollar')print('Todo por desarrollar')
+print('Todo por desarrollar')
